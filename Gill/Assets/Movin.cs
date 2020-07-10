c00dfca7fb1b175f7a665723345290cd1f8725c8
Joey <joeyecorbett@gmail.com>
2020-07-10 02:05:09 +0100

    Added basic sprint + cleaned up code

--- Gill/Assets/Movin.cs
+++ Gill/Assets/Movin.cs
@@ -3,30 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 //movin right along
-public class Movin : MonoBehaviour
-{
-    public float moveSpeed = 5f;
-    public float jumpHeight = 10f;
+
+public class Movin : MonoBehaviour {
+    public float moveSpeed;
+    public float jumpHeight;
     public bool runAnimate;
     private Rigidbody2D rigidbody;
     public Animator animator;
 
-    void Start()
-    {
+    void Start() {
         rigidbody = transform.GetComponent<Rigidbody2D>();
         animator.SetBool("runAnimate", false);
-        jumpHeight = 10f;
     }
 
 
-    void Update()
-    {
+    void Update() {
+        if(Input.GetButton("Sprint")) {
+            moveSpeed = 7f;
+        } else {
+            moveSpeed = 5f;
+        }
 
-    }
-
-
-  private void FixedUpdate()
-    {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
         
@@ -44,9 +41,15 @@ public class Movin : MonoBehaviour
                 {
             animator.SetBool("runAnimate", false);
         }
+
         if (Input.GetButtonDown("Jump"))
         {
             rigidbody.AddForce(jumpHeight * Vector2.up, ForceMode2D.Impulse);
         }
+    }
+
+
+  private void FixedUpdate() {
+
     }
 }
