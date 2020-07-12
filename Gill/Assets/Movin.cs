d6cf23c6dc4f7f9dd0d9ebc29023fd3bb340e56e
Joey <joeyecorbett@gmail.com>
2020-07-12 01:08:30 +0100

    Jump animation

--- Gill/Assets/Movin.cs
+++ Gill/Assets/Movin.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using System;
 //movin right along
 
-public class Movin : MonoBehaviour {
-    public float moveSpeed = 5f;
-    public float jumpHeight = 10f;
+public class Movin : MonoBehaviour { 
+    //just to note, its good to initialize varaibles here but not give them a value. 
+    //Set them inside the editor or inside Start()
+    public float moveSpeed;
+    public float jumpHeight;
     public bool runAnimate;
     private Rigidbody2D rigidbody;
     public Animator animator;
@@ -15,21 +17,29 @@ public class Movin : MonoBehaviour {
     void Start() {
         rigidbody = transform.GetComponent<Rigidbody2D>();
         animator.SetBool("runAnimate", false);
+        animator.SetBool("jumpAnimate", false);
+        animator.SetBool("crawlAnimate", false);
+        animator.SetBool("kickAnimate", false);
         jumpHeight = 10f;
+        moveSpeed = 5f;
     }
 
 
     void Update() {
-        if(Input.GetButton("Sprint")) {
+
+        //Sprint with shift
+        if(Input.GetButton("Sprint")) { 
             moveSpeed = 7f;
         } else {
             moveSpeed = 5f;
         }
 
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
-        
 
+        //Moves with left & right arrow, or A & D
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;       
+
+        
         if (Input.GetAxis("Horizontal") < -0.1)
         {
             animator.SetBool("runAnimate", true);
@@ -47,12 +57,18 @@ public class Movin : MonoBehaviour {
             animator.SetBool("runAnimate", false);
         }
 
+        //Jump with up arrow
         if (Input.GetButtonDown("Jump"))
         {
+            //Check if player is grounded
             if (isGrounded)
             {
+                animator.SetBool("jumpAnimate", true);
                 rigidbody.AddForce(jumpHeight * Vector2.up, ForceMode2D.Impulse);
-            }
+            } 
+        }
+        else {
+                animator.SetBool("jumpAnimate", false);
         }
     }
 
