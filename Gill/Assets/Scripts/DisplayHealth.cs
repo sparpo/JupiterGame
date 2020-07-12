e1dd4a025d33d3137c983a23ea4d81cd2332b5f0
Joey <joeyecorbett@gmail.com>
2020-07-12 17:05:19 +0100

    Basic health display
    Added a player stats script to gillbert and UI elements for dispalying health. Health can be depleted using space

---
+++ Gill/Assets/Scripts/DisplayHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gillbert;
+    private int health;
+    public Text healthText;
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = gillbert.GetComponent<GillbertStats>().getHealth();
+        healthText.text = "Health: " + health + "%";
+    }
+}
---
+++ Gill/Assets/Scripts/GillbertStats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GillbertStats : MonoBehaviour
+{
+    public int health;
+    private const int maxHealth = 100; // cannot be changed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health>maxHealth) {
+            health = maxHealth;
+        }
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            health--;
+        }
+    }
+    
+    public void setHealth(int h) {
+        health = h;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public void changeHealth(int amount) {
+        health+= amount;
+    }
+}
