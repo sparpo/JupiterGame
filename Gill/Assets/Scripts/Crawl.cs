f69a01bd7c7b26e876659126f1ba529e6bee45fd
Conlaed <iamconlaed@gmail.com>
2020-07-26 00:32:00 +0100

    Crawling
    Couldn't figure out how to commit the game objects to prefabs, they didn't seem to show the option. Crawling within one unit works though

---
+++ Gill/Assets/Scripts/Crawl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crawl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Crouch"))
+        {
+            this.GetComponent<Collider2D>().enabled = true;
+        }
+        else
+        {
+            this.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
