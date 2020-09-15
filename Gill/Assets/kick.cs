9cc7728009ac869846ede1b570f1f4c313d962c3
Joey <joeyecorbett@gmail.com>
2020-09-16 00:18:27 +0100

    kick collider

---
+++ Gill/Assets/kick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kick : MonoBehaviour
+{
+        public float attackTime;
+    public float startTimeAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if( attackTime <= 0 )
+        {
+            
+            if (Input.GetButton("Fire1"))
+            {
+                this.GetComponent<Collider2D>().enabled = true;
+            }
+            attackTime = startTimeAttack;
+            
+        }
+        else
+        {
+            this.GetComponent<Collider2D>().enabled = false;
+            attackTime -= Time.deltaTime;
+
+        }
+    }
+}
